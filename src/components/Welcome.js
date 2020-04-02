ee5b1d105e50a058b9e99fe6a7d3415fe15b93a6
Jonathan Adler <jradler@gmail.com>
2020-04-02 13:41:43 -0700

    remove logout button from Welcome, as it's in the nav now

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,24 +9,11 @@ const Welcome = (props) => {
     const [state, setState] = React.useContext(AuthContext);
     console.log(state);
 
-    // //TODO: move this bit to a nav bar
-    // if(!state.user) {
-    //     return (
-    //         <Redirect to='/login' push={true} />
-    //     )
-    // }
-    // async function handleLogout() {
-    //     await Auth.signOut();
-    //     setState({...state, user: null});
-    // }
-
     return (
         <div>
            <Navbar />
-           
             <div className="container mt-4">
                 <h2>Welcome, {state.user.attributes.name}!</h2>
-                <button type="button" className="btn btn-primary" >Log Out</button>
             </div>
         </div>
     )
